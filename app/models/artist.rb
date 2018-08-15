a3b1aced5bcda9c4b25001d3c42429eec058098e
Marylene Chan <marylenechan@Marylenes-MacBook-Pro-2.local>
2018-08-14 21:24:39 -0500

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,2 +1,10 @@
 class Artist < ActiveRecord::Base
+    has_many :songs
+
+    # for each instance of an artist count the songs that belong to this artist
+    # access to the songs because artist has many songs
+    def song_count
+      songs.size
+    end
+
 end
