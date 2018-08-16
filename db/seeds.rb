bb9b08da1ead7891ed0ed3277965a0c1abfe10bd
github username <github email address>
2018-08-15 19:08:59 -0500

    First Commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+Artist.delete_all
+Song.delete_all
+
+brittney = Artist.create(name: "Brittney Spears")
+travis = Artist.create(name: "Travis Scott")
+snoop = Artist.create(name: "Snoop Dog")
+calvin = Artist.create(name: "Calvin Harris")
+
+Song.create(title: "Oops I did it again" , artist_id: brittney.id)
+Song.create(title: "Houston something", artist_id: travis.id)
+Song.create(title: "Weed something", artist_id: snoop.id)
+Song.create(title: "I'm back", artist_id: brittney.id)
